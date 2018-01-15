27390920f18579439c2feb8fd194dbb022e73059
MRMRMRMayInternship <34727895+MRMRMRMayInternship@users.noreply.github.com>
2018-01-15 17:10:41 +0900

    web addition

---
+++ Web-prescription/WebPIPDemo/WebDemo/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebDemo.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public bool GetCheckResult(string id, string pw)
+        {
+            var result = false;
+            if (id.Equals("root") && pw.Equals("roor"))
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
---
+++ Web-prescription/WebPIPDemo/WebDemo/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebDemo.Models;
+using System.Web.Mvc;
+namespace WebDemo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]{
+            new Product{ ID = 1, Name="Tomato Soup", Category = "Groceries", Price=1},
+            new Product{ ID = 2, Name="Yo-yo", Category = "Toys", Price=3.75M},
+            new Product{ ID = 3, Name="Hammer", Category = "Hardware", Price=16.99M}
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        public IEnumerable<Product> GetProductsByName(string name)
+        {
+            var _products = products.Where(p => p.Name.Contains(name));
+            if (_products.Count() <= 0)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return _products;
+        }
+        public Product GetProductById(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.ID.Equals(id));
+            if (product == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return product;
+        }
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return products.Where((p) => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
